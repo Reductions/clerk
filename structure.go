2115ef08d491c52c68cc363b7df8a879ae72de20
Reductions <slavi.boyanov@gmail.com>
2017-01-17 17:13:59 +0200

    Finished the rest of the traces types and added some functions

--- structure.go
+++ structure.go
@@ -3,14 +3,48 @@ Package clerk is a go library for working with InkML dataformat
 */
 package clerk
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"image"
+)
+
+// A Document is the wrapping type of any InkML
+type Document struct {
+	Version string
+	Root    *Ink
+}
 
 // Node is interface that gives the base for all valid InkML tags
 type Node interface {
+	Parent() *Node
+	Children() []*Node
+	AddChild(newChild *Node) error
+	AddChildAfter(newChild *Node, refChild *Node) error
+	AddChildBefor(newChild *Node, refChild *Node) error
 }
 
 // An Ink element is the root element of any InkML instance
 type Ink struct {
 	Children   []*Node     // Child nodes.
 	Attributes []*xml.Attr // Node attributes.
+}
+
+// New creates InkML document with just Ink tag inside it
+func New() *Document {
+	return nil
+}
+
+// ParseBytes creates an InkML document from byte slice
+func ParseBytes(bytes []byte) (*Document, error) {
+	return nil, nil
+}
+
+// ToBytes serialises the dicument as byte slice
+func (*Document) ToBytes() []byte {
+	return nil
+}
+
+// ToImage writes the InkML to and Image
+func (*Document) ToImage(img *image.Image) error {
+	return nil
 }
--- trace.go
+++ trace.go
@@ -2,9 +2,9 @@ package clerk
 
 import "encoding/xml"
 
-// A Channle defines an imput channle to be recorded
+// A Channel defines an imput channle to be recorded
 type Channel struct {
-	Intermittent bool   // Defines whether the Channel
+	Intermittent bool   // Defines whether the Channel is Intermittent
 	Default      string // Default value of the Channle if Intermittent is true
 	Name         string // Name of the channle
 	Type         string // Type of the channle
@@ -12,9 +12,34 @@ type Channel struct {
 	Mapping      string // Mapping type
 }
 
+// TraceNode is interface that gives the base for every element that could be grouped into a TraceGroups
+type TraceNode interface {
+}
+
 // A TraceFormat is element that describes the format used to encode points
 type TraceFormat struct {
 	Children   []*Channel  // Child nodes.
 	Attributes []*xml.Attr // Node attributes.
 	Parent     *Node       // Parent node.
+}
+
+// A Trace is the basic element used to record the the trajectory of a pen
+type Trace struct {
+	Attributes []*xml.Attr // Node attributes.
+	Parent     *Node       // Parent node.
+	Value      string      // The value of the Trace
+}
+
+// A TraceGroup provides mechanisms to gather and combine traces into collections
+type TraceGroup struct {
+	Children   []*TraceNode // Child nodes.
+	Attributes []*xml.Attr  // Node attributes.
+	Parent     *Node        // Parent node.
+}
+
+// A TraceView element is used to include traces by reference
+type TraceView struct {
+	Children   []*TraceNode // Child nodes.
+	Attributes []*xml.Attr  // Node attributes.
+	Parent     *Node        // Parent node.
 }
